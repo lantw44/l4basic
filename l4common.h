dc258e02720308981a4f8a2e4848c582c1b71123
LAN-TW <lantw44@gmail.com>
2013-12-07 18:30:38 +0800

    Add macros to define getters and setters

--- l4common.h
+++ l4common.h
@@ -16,10 +16,19 @@
 # define bool  char
 # define true  1
 # define false 0
+# define inline
 # define LBS_COMMON_ISO_C89
 # define LBS_COMMON_CHECK_TYPE(x,type) (x)
 #endif /* __STDC_VERSION__ */
 
 #define LBS_COMMON_NULL_PTR ((void*)NULL)
+#define LBS_COMMON_DEFINE_GETTER(ns,xt,m,mt) \
+    static inline mt ns ## _get_ ## m (xt x) { \
+        return x->m; \
+    }
+#define LBS_COMMON_DEFINE_SETTER(ns,xt,m,mt) \
+    static inline void ns ## _set_ ## m (xt x, mt v) { \
+        x->m = v; \
+    }
 
 #endif /* LBS_COMMON_H */
